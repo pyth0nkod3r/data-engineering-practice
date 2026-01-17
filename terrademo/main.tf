b4e20cd89b6d6654030d6a4f99d943b3801df715
Miracle <o.amagwula@stu.unizik.edu.ng>
2026-01-17 17:26:37 +0000

    added terraform and gitignore

---
+++ terrademo/main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "7.16.0"
+    }
+  }
+}
+
+provider "google" {
+  # Configuration options
+  project = "macmkboy-481120"
+  region  = "us-central1"
+}
